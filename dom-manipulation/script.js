29b9107bed9a72b5af096b95bf82e9e8318f55c9
IyvonneAyoyi <iyvonneayoyi@gmail.com>
2025-08-11 22:28:01 +0300

    Add .js file with quotes variable

---
+++ dom-manipulation/script.js
@@ -0,0 +1,11 @@
+const quotes = [
+  // Motivational Quotes
+  {text: "Code is like humor. When you have to explain it, it’s bad. — Cory House", category: "motivational"},
+  {text: "Don’t stop when you’re tired. Stop when you’re done.", category: "motivational"  },
+  { text: "Every great developer you know got there by solving problems they were unqualified to solve until they actually did it." , category: "motivational"},
+  //Inspirational
+  {text: "The best way to predict the future is to invent it. — Alan Kay" ,category: "inspirational"},
+  {text: "Simplicity is the soul of efficiency. — Austin Freeman" ,category: "inspirational"},
+  { text: "Learning to write programs stretches your mind, and helps you think better. — Bill Gates" ,category: "inspirational"}
+];
+
