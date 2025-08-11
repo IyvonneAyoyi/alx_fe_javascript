8f4b7d0bc4826b3ddc22e4cad5e407f163f55447
IyvonneAyoyi <iyvonneayoyi@gmail.com>
2025-08-12 02:26:29 +0300

    Implement importFromJsonFile to read JSON quotes file

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -128,3 +128,14 @@ function createAddQuoteForm() {
 };
 
  createAddQuoteForm();
+
+ function importFromJsonFile(event) {
+    const fileReader = new FileReader();
+    fileReader.onload = function(event) {
+      const importedQuotes = JSON.parse(event.target.result);
+      quotes.push(...importedQuotes);
+      saveQuotes();
+      alert('Quotes imported successfully!');
+    };
+    fileReader.readAsText(event.target.files[0]);
+  }
