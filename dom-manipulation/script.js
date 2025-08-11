98e61af41a4365befc70335c04cc984639e013d1
IyvonneAyoyi <iyvonneayoyi@gmail.com>
2025-08-12 01:49:26 +0300

    Load quotes from localStorage on page load

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -1,13 +1,13 @@
-const quotes = [
-  // Motivational Quotes
+const storedQuotes = localStorage.getItem('quotes');
+const quotes = storedQuotes ? JSON.parse(storedQuotes) : [
   {text: "Code is like humor. When you have to explain it, it’s bad. — Cory House", category: "motivational"},
-  {text: "Don’t stop when you’re tired. Stop when you’re done.", category: "motivational"  },
-  { text: "Every great developer you know got there by solving problems they were unqualified to solve until they actually did it." , category: "motivational"},
-  //Inspirational
+  {text: "Don’t stop when you’re tired. Stop when you’re done.", category: "motivational"},
+  {text: "Every great developer you know got there by solving problems they were unqualified to solve until they actually did it." , category: "motivational"},
   {text: "The best way to predict the future is to invent it. — Alan Kay" ,category: "inspirational"},
   {text: "Simplicity is the soul of efficiency. — Austin Freeman" ,category: "inspirational"},
-  { text: "Learning to write programs stretches your mind, and helps you think better. — Bill Gates" ,category: "inspirational"}
+  {text: "Learning to write programs stretches your mind, and helps you think better. — Bill Gates" ,category: "inspirational"}
 ];
+
 
 
 
@@ -43,8 +43,11 @@ function createAddQuoteForm() {
     //Get the container div where the form will be added
     const quoteFormContainer = document.getElementById('formContainer');
 
+
     //Create form element
     const form = document.createElement('form');
+
+
     // input for quote text
     const inputText = document.createElement('input');
     inputText.id ='newQuoteText';
